27728793bc83610b6bcd29a9fefc5cf36bfc725a
Aman Kurmi <59317533+sarfiiraa@users.noreply.github.com>
2026-04-19 11:55:33 +0530

    Fix comment typo in add method

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
 	 * @return int
 	 */
 	public int add(int i, int j){
-		return i+j; //Adding two value
+		return i+j; //Adding two values
 	}
 	public int multiply(int i, int j){
 		return i*j;
